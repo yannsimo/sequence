3e795bb06a8e4dc2e6c89dcb771e1c6e67bc0436
Yann <yannjuniorsim@gmail.com>
2025-03-10 12:10:10 +0100

    of

--- sequence/settings.py
+++ sequence/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-7_5ryyl7-^dpw*tuw1j-w3+gm2p3)%f9(&npghr6clk*pw)h(9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['KeviCisc.pythonanywhere.com']
 
 
 # Application definition
@@ -118,7 +118,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
